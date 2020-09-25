5c5ca748a2e6e905975bdc1954cead5b26d4f675
Miguel Prada <mapra99@gmail.com>
2020-09-24 20:09:15 -0500

    map styling

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,14 @@
+// library dependencies
 import React from 'react';
 
+// assets
+import mapImg from '../assets/static/images/placehodler-map.jpg';
+import '../assets/styles/components/Map.scss';
+
 const Map = () => (
-  <div></div>
+  <div className='map-wrap'>
+    <img className='map-img' src={mapImg} alt='placeholder map' />
+  </div>
 );
 
 export default Map;
