8d9448ad2535f07eed20f221d1d2152035a68c58
Miguel Prada <mapra99@gmail.com>
2020-09-27 17:14:56 -0500

    removed marker

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -14,15 +14,6 @@ const MyMap = ({lat, lng}) => {
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={location}>
-        <Popup>
-          A pretty CSS3 popup.
-          {' '}
-          <br />
-          {' '}
-          Easily customizable.
-        </Popup>
-      </Marker>
     </Map>
   );
 };
